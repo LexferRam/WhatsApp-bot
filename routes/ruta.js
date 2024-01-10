34c81204014969c4f67fbd730c05ff0a37170464
Lexfer Ramirez <lexferramirez@gmail.com>
2024-01-10 16:46:48 -0400

    update9

--- routes/ruta.js
+++ routes/ruta.js
@@ -2,7 +2,8 @@ const express = require("express")
 const router = express.Router()
 const apicontroller = require("../controllers/apicontroller")
 
-router.get("/",apicontroller.verificar)
-router.post("/",apicontroller.recibir)
+router
+.get("/",apicontroller.verificar)
+.post("/",apicontroller.recibir)
 
 module.exports = router
