1355b90feb94f16b878c8aaffaac2a11704818e6
Lexfer Ramirez <lexferramirez@gmail.com>
2024-01-10 14:50:59 -0400

    update3

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,20 +1,20 @@
 const verificar = (req, res) => {
-    try {
+    try{
         var tokenprojectwsp = "tokenwspproject"
-        var token = req.query["hub.verify_token"]
-        var challenge = req.query["hub.challenge"]
+        var token = req.query['hub.verify_token']
+        var challenge = req.query["hub.challenge"] 
 
-        if (challenge != null && token != null && token === tokenprojectwsp) {
+        if(challenge != null && token != null && token == tokenprojectwsp ){
             res.send(challenge)
-        } else {
-            res.status(400).send()
+        }else{
+            res.status(400).send();
         }
-        console.log(req)
-    } catch (e) {
-        res.status(400).send()
+
+    }catch(e){
+        res.status(400).send(); 
     }
-    res.send("Verificado")
 }
+
 
 const recibir = (req, res) => {
     res.send("Recibido")
