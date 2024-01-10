cd85dc503cccf98bf34ffe547927dae46f90397b
Lexfer Ramirez <lexferramirez@gmail.com>
2024-01-10 16:55:41 -0400

    update12

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -1,38 +1,37 @@
+const sendMessage = require("../service/apiService")
 const verificar = (req, res) => {
-    try{
-        var tokenprojectwsp = "tokenwspproject"
-        var token = req.query['hub.verify_token']
-        var challenge = req.query["hub.challenge"] 
-
-        if(challenge != null && token != null && token == tokenprojectwsp ){
+    try {
+        var tokenapi = "TOKENAPINODE"
+        var token = req.query["hub.verify_token"]
+        var challenge = req.query["hub.challenge"]
+        if(challenge != null && token != null && token == tokenapi){
             res.send(challenge)
         }else{
             res.status(400).send();
+
         }
-
-        console.log(req)
-
-    }catch(e){
-        res.status(400).send(); 
+    } catch (error) {
+        res.status(400).send();
     }
 }
 
-
 const recibir = (req, res) => {
-    console.log('aquiiiiiii')
     try {
-        var entry = (req.body["entry"])[0];
-        var changes = (entry["changes"])[0];
-        var value = changes["value"];
-        var objetoMensaje = value["messages"]
+        const entry = (req.body["entry"])[0]
+        const changes = (entry["changes"])[0]
+        const value = changes["value"]
+        const objMessage = value["messages"]
 
-        console.log(value)
-        res.send("EVENT_RECEIVED")
+        const messages = objMessage[0]
+        const number = messages["from"]
+        const text = messages["text"]["body"]
+        console.log(`El numero es ${number} y el texto es ${text}`)
+        sendMessage.sendMenssageWhatsapp(text,number)
+        res.send("EVENT_RECEIVED"); 
 
     } catch (error) {
-        console.log(req)
-        res.send("EVENT_RECEIVED")
-        
+        console.log(error)
+        res.send("EVENT_RECEIVED"); 
     }
 }
 
